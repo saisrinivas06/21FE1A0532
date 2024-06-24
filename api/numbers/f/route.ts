0cf3aa06af8a550d7cd0df56aa72fe8aff6e886a
saisrinivas06 <130163566+saisrinivas06@users.noreply.github.com>
2024-06-24 12:08:59 +0530

    Add files via upload

---
+++ api/numbers/f/route.ts
@@ -0,0 +1,10 @@
+const url = "http://20.244.56.144/test/f";
+
+const data = fetch(url)
+   .then(res => {
+    if(res) {
+        console.log("hello");
+        return res.json();
+    }
+   })
+   .catch(err => console.log("err"));
